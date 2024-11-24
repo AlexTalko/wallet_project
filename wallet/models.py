23c4577441d674b127500d64ac65885c35651d92
air <talko.alexander@gmail.com>
2024-11-25 01:13:04 +0400

    add models.py

--- wallet/models.py
+++ wallet/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.db import transaction
 
-# Create your models here.
+class Wallet(models.Model):
+    uuid = models.UUIDField(primary_key=True)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    @transaction.atomic
+    def deposit(self, amount):
+        self.balance += amount
+        self.save()
+
+    @transaction.atomic
+    def withdraw(self, amount):
+        if amount > self.balance:
+            raise ValidationError('Insufficient funds')
+        self.balance -= amount
+        self.save()
